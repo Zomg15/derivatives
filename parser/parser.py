4a5fe137a0dfa2b514096a5ddaa3f68b358711f7
Zomg15 <43176997+Zomg15@users.noreply.github.com>
2024-01-01 17:27:43 +0200

    added support for operations

--- parser/parser.py
+++ parser/parser.py
@@ -40,7 +40,7 @@ def parse(original_array):
     print(f"currently parsing: {tree}")
     # If the top-level operation is a function, set that to the root of the tree and the rest to the branch, then parse that
     if tree[0] in ["sin", "cos", "tan", "log", "ln", "sqrt"] and tree[1] == "(":
-        print("we're here")
+        print("function found at beginning")
         depth = 0
         end_index = 0
         has_nested_parentheses = False
@@ -80,7 +80,7 @@ def parse(original_array):
     has_nested_parentheses = False
     topmost_end_index = 0
     topmost_start_index = 0
-    print("but now we're here")
+    print("parsing parentheses")
     for i, token in reversed(list(enumerate(tree))):
         if token == ")":
             print(") found")
@@ -99,12 +99,34 @@ def parse(original_array):
                 topmost_start_index = i
             print(topmost_start_index, topmost_end_index)
             topmost_parentheses = tree[topmost_start_index:topmost_end_index+1]
-            del tree[topmost_start_index:topmost_end_index]
+            del tree[topmost_start_index:topmost_end_index+1]
             tree.insert(topmost_start_index, parse(topmost_parentheses))
             has_nested_parentheses = False
     
-    return tree
-        
+    # Now that parentheses are dealt with, we can do the operations
+    # Go by order of operations (exponents, multiplication/division, addition/subtraction), bring together left and right operands
+    # When deleting operator and left/right branches, the index you have to reinsert the tree in is index - 1 (the place of the left operand)
+    
+    print("parsing operations")
+    operation_phase = 0
+    operators = [["^"], ["*", "/"], ["+", "-"]]
+    while operation_phase <= 2:
+        for i, token in enumerate(tree):
+            if type(token) is str and token in operators[operation_phase]:
+                print(f"found operator {token}")
+                print(f"left operand: {tree[i-1]}")
+                print(f"right operand: {tree[i+1]}")
+                operation_tree = Tree(token, tree[i-1], tree[i+1])
+                del tree[i-1:i+2]
+                tree.insert(i-1, operation_tree)
+        operation_phase += 1
 
+    if type(tree) is list:
+        print(f"ended up with list {tree}")
+        return tree[0]
+    else:
+        return tree
+        
+# sqrt(x) + ln(x+1)
 parsed_tree = begin_parse(['sqrt', '(', 'x', ')', '+', 'ln', '(', 'x', '+', '1', ')'])
 print(parsed_tree)
